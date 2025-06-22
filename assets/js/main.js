3ba3030aafbe424b47057134b40f2ee91ccd73e1
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-06-22 21:24:25 +0700

    revesed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 // chi ap dung voi backtick
 let userName = "Tran Van A";
-let message = `Xin chao ${userName}`;
+let message = `Xin chao ${10 + 10}`;
 console.log(message);
+
+
+function reverseString (str) {
+    let reversed = ""
+   for (let i = str.length - 1 ; i >= 0 ; i--) {
+    reversed += str[i]
+   }
+   return reversed
+}
