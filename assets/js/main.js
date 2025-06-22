18a8be4169471e975b894ec001b486e0819e5fc4
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-06-22 21:47:51 +0700

    tim ucln bang thuat toan uclide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
-// chi ap dung voi backtick
-let userName = "Tran Van A";
-let message = `Xin chao ${10 + 10}`;
-console.log(message);
+function timUCLN(a,b) {
+    if ( b > a) {
+       const temp = a;
+       a = b;
+       b = temp;
+    }
 
-
-function reverseString (str) {
-    let reversed = ""
-   for (let i = str.length - 1 ; i >= 0 ; i--) {
-    reversed += str[i]
-   }
-   return reversed
+    while(b != 0) {
+        const temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
+ console.log(timUCLN(48,60));
