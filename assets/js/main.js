58f16520a1311c4813901c5ce38f0d15451598ff
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-06-23 08:29:14 +0700

    bt vong lap

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,51 @@ function timUCLN(a,b) {
     return a;
 }
  console.log(timUCLN(48,60));
+
+
+
+ function countGreaterThan (arr , value) {
+    let count = 0;
+    for(let i = 0 ; i < arr.length ; i++) {
+       if(arr[i] > value) {
+        count ++;
+       }
+    }
+    return count;
+}
+
+function alternateSum(arr) {
+    let total = 0;
+    for(let i = 0 ; i < arr.length;i++) {
+        total += (i % 2 === 0) ? arr[i] : -arr[i];
+    }
+    return total;
+}
+
+function sumOfSquares (arr) {
+    let sum = 0;
+    for(let i = 0 ; i < arr.length ; i++) {
+        sum += arr[i] * arr[i];
+    }
+    return sum;
+}
+
+function filterEvensAndSum(arr) {
+    let sum = 0;
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(arr[i] % 2 === 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+function findLongestWord(arr) {
+    let string = '';
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(arr[i].length > string.length) {
+            string = arr[i];
+        }
+    }
+    return string;
+}
