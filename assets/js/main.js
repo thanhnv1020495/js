400ba77d5dd928d867ac2b24247e13fb55dffcb4
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-10-04 09:41:32 +0700

    Pass by value và Pass by reference

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-const obj = {a: 1,  b: 2 , c: 3};
-console.log(Object.entries(obj));
-// cung cấp một cách thức để duyệt qua cả key và value của một object
+//Truyền theo giá trị
+//1 sao chép giá trị của biến : let y = x;
+//2 truyền bản sao vào bên trong hàm 
+//3 Đặc điểm : gán lại tham số trong hàm , không làm thay đổi biến bên ngoài 
+
+function changeValue(y) {
+    console.log(y);
+    y = 20;
+}
+
+let  x = 10;
+changeValue(x);
+
+console.log(x);
 
 
 
 
+// Truyền trực tiếp tham chiếu của biến vào bên trong hàm 
+// đặc điểm gán lại tham số trong hàm , ngay lập tức biến ngoài củng thay đổi 
+function changeValue(y) {
+    y = {name: 'bob'};
+    console.log(y);
+}
+
+x = {name: 'john'};
+changeValue(x);
 
 
