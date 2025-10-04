0261d0c06ad555cac0bfe4cae91ced1b5f98ce7e
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-10-04 19:39:33 +0700

    object prototypes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,14 @@
-const user = {
-    firstName: "A",
-    lastName: "Nguyen Van",
-    fullName() {
-        return `${this.lastName} ${this.firstName}`;
-    },
-    set reName(newName) {
-        this.firstName = newName.split(" ").pop();   // lấy từ cuối
-        this.lastName = newName.split(" ").slice(0, -1).join(" ");
-    },
-    get reName() {
-        return `${this.lastName} ${this.firstName}`;
-    }
+Number.prototype.moneyFomart = function() {
+    return `${this}d`
 }
 
-console.log(user.fullName());  // "Nguyen Van A"
 
-user.reName = "Nguyen Van B"; // dùng setter để đổi tên
-console.log(user.reName);      // "Nguyen Van B"
-console.log(user.fullName());  // "Nguyen Van B"
+let number = new Number(999);
+console.log(number.moneyFomart());
+
+
+
+
+
 
 
