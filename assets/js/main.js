81913c8ccff3cd6ec8a13919f1d43cb67b1dcf10
thanhnv1020495 <vn4956599nguyenvanthanh@gmail.com>
2025-10-04 13:30:18 +0700

    getter and setter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,22 @@
-//Truyền theo giá trị
-//1 sao chép giá trị của biến : let y = x;
-//2 truyền bản sao vào bên trong hàm 
-//3 Đặc điểm : gán lại tham số trong hàm , không làm thay đổi biến bên ngoài 
-
-function changeValue(y) {
-    console.log(y);
-    y = 20;
+const user = {
+    firstName: "A",
+    lastName: "Nguyen Van",
+    fullName() {
+        return `${this.lastName} ${this.firstName}`;
+    },
+    set reName(newName) {
+        this.firstName = newName.split(" ").pop();   // lấy từ cuối
+        this.lastName = newName.split(" ").slice(0, -1).join(" ");
+    },
+    get reName() {
+        return `${this.lastName} ${this.firstName}`;
+    }
 }
 
-let  x = 10;
-changeValue(x);
+console.log(user.fullName());  // "Nguyen Van A"
 
-console.log(x);
-
-
-
-
-// Truyền trực tiếp tham chiếu của biến vào bên trong hàm 
-// đặc điểm gán lại tham số trong hàm , ngay lập tức biến ngoài củng thay đổi 
-function changeValue(y) {
-    y = {name: 'bob'};
-    console.log(y);
-}
-
-x = {name: 'john'};
-changeValue(x);
+user.reName = "Nguyen Van B"; // dùng setter để đổi tên
+console.log(user.reName);      // "Nguyen Van B"
+console.log(user.fullName());  // "Nguyen Van B"
 
 
